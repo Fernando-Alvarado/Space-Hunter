f4dfa217fef773d94e9ce7dbe538207d72e4bd9d
fer <fernandoalvarado@comunidad.unam.mx>
2019-12-30 13:58:39 -0600

    Subiendo mis avances

--- Basura que i need/ImDoingMagic.js
+++ Basura que i need/ImDoingMagic.js
@@ -17,11 +17,46 @@ function NumerosAleatorios(tope){
   
 return MatrizPrincipal;
 }
-var Runni = ArrayBaseDeLaNaves(5);
+var original;
+class gossip{
+    constructor (RunnigGame, number, ejex, ejey) {     
+      //  this.original = original; //sera la matriz que se pase por parametro de la funcion              
+        this.RunnigGame = RunnigGame;  //matriz modificada
+        this.number = number; //numero en el eje y 
+        this.ejex1 = ejex1;  //valores de sus posiciones que es lo que quiero sacar
+        this.ejex2 = ejey2;  //
+     }
+     suck(){
+//       original[this.number][]=   this.RunnigGame[this.number][]    
+    }
+
+ }
+var Runni = ArrayBaseDeLaNaves(4);
+console.log(Runni)
+function pedorra(matriz, position){
+    //console.log()
+  matriz[position][1] +=30
+  console.log(matriz)
+
+}
+
+console.log('-------------')
+pedorra(Runni, 2)
 /*
 Crear una cookie
 document.cookie = “name=valor”;
 document.cookie = “name=valor; expires=fecha en UTC”;
 Obtener cookies
 document.cookie; //obtiene una cadena con todas las cookies
+///////--------------------------------------------------------------------------------------------
+///////--------------------------------------------------------------------------------------------
+class PersonajePrincipal{
+       constructor (matrizDondeSeTrabaja) {           
+            this.matrizDondeSeTrabaja = matrizDondeSeTrabaja;         
+        }
+        mover(){
+           
+         
+        }
+    }
 */
