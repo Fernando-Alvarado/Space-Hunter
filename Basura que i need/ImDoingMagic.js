20ab060e592b2da8a189406d6e665473ec8803a8
fer <fernandoalvarado@comunidad.unam.mx>
2020-01-01 18:46:19 -0600

    Subiendo mas cosas

--- Basura que i need/ImDoingMagic.js
+++ Basura que i need/ImDoingMagic.js
@@ -17,31 +17,66 @@ function NumerosAleatorios(tope){
   
 return MatrizPrincipal;
 }
-var original;
-class gossip{
-    constructor (RunnigGame, number, ejex, ejey) {     
-      //  this.original = original; //sera la matriz que se pase por parametro de la funcion              
-        this.RunnigGame = RunnigGame;  //matriz modificada
-        this.number = number; //numero en el eje y 
-        this.ejex1 = ejex1;  //valores de sus posiciones que es lo que quiero sacar
-        this.ejex2 = ejey2;  //
+////----------------------------------------------------------------------------------------
+///----------------En produccion para exportar a juego.js-----------------------------------
+////----------------------------------------------------------------------------------------
+class balas{
+    //matriz donde corre el juego || cordenadas conde empezo a disparar || coordenadas a donde va
+    constructor(PrincipalMat, whereX, whereY, ObjX, ObjY, Killed){ //kileed se refiere a la nave a
+        //que se le espero disparas, no se si quieran que se permita el fuego amigo    
+     this.Matriz = PrincipalMat;
+     this.WherX = whereX;
+     this.WherY = whereY;
+     this.ObjX = ObjX;
+     this.ObjY = ObjY;
+     this.Killed = Killed;
      }
-     suck(){
-//       original[this.number][]=   this.RunnigGame[this.number][]    
-    }
-
+    MidnightBlame(){
+        function GodsLoop(){
+            setTimeout(function(){ 
+                       
+                GodsLoop();
+            }, 100);
+        }
+        GodsLoop(); 
+    }  
  }
-var Runni = ArrayBaseDeLaNaves(4);
-console.log(Runni)
-function pedorra(matriz, position){
-    //console.log()
-  matriz[position][1] +=30
-  console.log(matriz)
-
+////---------------------------------------------------------------------------------------------------
+///----------------------------------------------------------------------------------------------------
+function graficadora(x1, y1, x2, y2){
+    var m = (y2-y1)/(x2-x1)
+    return m;
 }
+var prueba = graficadora(2,3,4,5)
 
-console.log('-------------')
-pedorra(Runni, 2)
+console.log('iono')
+
+for (i=2; i<=10; i++){
+    var yo = (prueba*i)+4;
+    console.log(yo)
+}
+console.log('io')
+
+
+
+//var Runni = ArrayBaseDeLaNaves(4);
+
+
+
+/*for (i=x1; i>=10; i++){
+    console.log((m*i)+y1)
+  }*/
+/*
+the gods loop
+   function GodsLoop(){
+                setTimeout(function(){ 
+                           
+                    GodsLoop();
+                }, 100);
+            }
+        MainBucle();
+        }
+*/
 /*
 Crear una cookie
 document.cookie = “name=valor”;
