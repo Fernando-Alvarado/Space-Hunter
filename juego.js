301ba34d8a9a7c725f19984e85c49063c01f24c7
Diegolibot <diegolibot@comunidad.unam.mx>
2020-01-03 20:02:00 -0600

    Feat: patrones de movimiendo y perseguir personaje para enemigos

--- juego.js
+++ juego.js
@@ -1,125 +1,130 @@
-//-------------DEclaracion de variables 
+//-------------DEclaracion de variables
 var limit = 50;//Numero de casillas de la matriz
 var tamCanvas= 500;//Tama;o del canvas
 var velocidad = 1000;//Esta variable dira que tan rapido las naves reacionaran
 var matrizPrincipal = document.getElementById('matrizBase');//puede ser o no el canvas principal xd
 var lienzoBase = matrizPrincipal.getContext('2d');
-var numnaves = 8; //numero de naves que hay declaradas 
+var numnaves = 10; //numero de naves que hay declaradas
+var rango = 7; //Nos dice que tanto ven las naves enemigas a su alrdedor
 var MatrizThatMakeMeCry = ArrayBaseDeLaNaves(numnaves);//tipo instanciando la matriz principal
+var patterns = new Array( //Array con todos los diferentes patrones, el primer número es la velocidad
+                  new Array(300,1,1,1,2,2,2),
+                  new Array(300,3,3,3,4,4,4),
+                  new Array(300,1,1,2,2,3,3),
+                  new Array(300,1,1,1,1,3,3,3,3,2,2,2,4,4,4),
+                  new Array(100,2,2,2,2,2,2,2,4,4,4,4,3),
+                  new Array(200,3,3,3,3,3,4,4,3,2,1,2,1),
+                  new Array(100,2,2),
+                  new Array(100,1,1),
+                  new Array(100,3,3),
+                  new Array(100,4,4),
+                  new Array(200,2,2,2,2,2,2,2),
+                  new Array(100,1,1,1,1,1,1,1),
+                  new Array(100,2,2,2,2,2,2,2),
+                  new Array(100,3,3,3,3,3,3,3),
+                  new Array(100,4,4,4,4,4,4,4)
+              );
+
+
 //--------------------------------OBJETOS------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------
 //Matriz[0][1], Matriz[0][2],
 class PersonajePrincipal{
-       constructor (matrizDondeSeTrabaja) {           
-            this.matrizDondeSeTrabaja = matrizDondeSeTrabaja;         
+       constructor (matrizDondeSeTrabaja) {
+            this.matrizDondeSeTrabaja = matrizDondeSeTrabaja;
         }
         mover(){
             var matrizDondeSeTrabaja = this.matrizDondeSeTrabaja;
             document.addEventListener('keydown', function(event) {//PARA RECONOCER LA tECLAS
-            var corriendo =  ChooseWhereToMove("black",matrizDondeSeTrabaja, matrizDondeSeTrabaja[0][2], matrizDondeSeTrabaja[0][1], event.code,"KeyW", "KeyS", "KeyA", "KeyD", "KeyE", 0);
+            var corriendo =  ChooseWhereToMove("black",matrizDondeSeTrabaja, matrizDondeSeTrabaja[0][2], matrizDondeSeTrabaja[0][1], event.code,"KeyW", "KeyS", "KeyA", "KeyD", 0);
             matrizDondeSeTrabaja[0][2]=corriendo[0];//se reinia la cuenta para que se pueda mover la neve
             matrizDondeSeTrabaja[0][1]=corriendo[1];//es algo idiota pero funciona jajajaja
-         
+
             });
         }
     }
 class NavesEnemigas{
-        constructor(matrizDondeSeTrabaja, number){//el 
+        constructor(matrizDondeSeTrabaja, number){//el
             this.workingMat = matrizDondeSeTrabaja;
             this.number = number;//indice dentro de la matriz principal donde se guardara la nave
-            this.numero =  NumerosAleatorios(5);//luego esto tengra que cambiar a 5 para que puedan
+            this.numero =  0;//luego esto tengra que cambiar a 5 para que puedan
+            this.rango = rango; //determina el rango de visión;
+            this.dist_player = 0; //Guarda la distancia de la nave al jugador
+            this.pattern_move = 0; //Determina el siguiente movimiento
+            this.current_pattern = NumerosAleatorios(8)-1; //Dice que patrón está haciendo actualmente
             //disparar
         }
         JustTheCreator(){//este metodo ara que las naves se muevan y  si tiempo que disparen
             //usara la funcion switch que cree
-            function MainBucle(velocidad, matriz, y, x ,move, ar1, ar2, ar3, ar4, ar5, position){ //Aqui se tendra que correr el bucle de las naves
-                setTimeout(function(){ 
-                    //El parametro primer parametro sera cambiado por quien disparo xd, pero siento que se tendra que pasar
-                    //un evento para disparar con el mouse
-                    var itsRunnig = ChooseWhereToMove("red",matriz, y, x, move, ar1, ar2, ar3, ar4, ar5, position);
-                   var numero = NumerosAleatorios(5);                  
-                    MainBucle(velocidad, matriz, itsRunnig[0], itsRunnig[1], numero, ar1, ar2, ar3, ar4, ar5, position);
+            function MainBucle(velocidad, matriz, y, x ,move, ar1, ar2, ar3, ar4, position,current_pattern,pattern_move,patterns){ //Aqui se tendra que correr el bucle de las naves
+                setTimeout(function(){
+                  this.dist_player=DistFromPlayer(matriz,y,x);
+                  //El parametro primer parametro sera cambiado por quien disparo xd, pero siento que se tendra que pasar
+                  //un evento para disparar con el mouse
+                  var numero = 0;
+                  if(this.dist_player <=rango){
+                    if(this.dist_player <= 3){
+                       console.log("pium");  //Aquí dispararía
+                       numero = 0;
+                    }
+                    else{
+                      numero = ChasePlayer(matriz,x,y);
+                      velocidad = 300;
+                    }
+                  }
+                  else{
+                    if(pattern_move == patterns[current_pattern].length){
+                      current_pattern = NumerosAleatorios(8)-1;
+                      pattern_move = 1;
+                    }
+                    numero=patterns[current_pattern][pattern_move];
+                    pattern_move++;
+                    velocidad = patterns[current_pattern][0];
+                  }
+
+
+                  var itsRunnig = ChooseWhereToMove("red",matriz, y, x, move, ar1, ar2, ar3, ar4, position);
+                  MainBucle(velocidad, matriz, itsRunnig[0], itsRunnig[1], numero, ar1, ar2, ar3, ar4, position,current_pattern,pattern_move,patterns);
                 }, velocidad);
             }
-        //console.log(this.number)
-        MainBucle(1000, this.workingMat, this.workingMat[this.number][2], this.workingMat[this.number][1], this.numero, 1, 2, 3, 4, 5, this.number);
-        }
-    }
-class balas{
-    //matriz donde corre el juego || cordenadas conde empezo a disparar || coordenadas a donde va
-    constructor(PrincipalMat, whereX, whereY, ObjX, ObjY, Killed, limiteCampoJuego){ //kileed se refiere a la nave a
-        //que se le espero disparas, no se si quieran que se permita el fuego amigo    
-        this.Matriz = PrincipalMat;//matris donde estan declaradas todas las naves
-        this.WherX = whereX;//coordenadas iniciales
-        this.WherY = whereY;
-        this.ObjX = ObjX;//cordenadas a lo que se le disparo
-        this.ObjY = ObjY;
-        this.Killed = Killed;///a que va a matar, si es nave enemiga o no
-        this.limCampJue = limiteCampoJuego;///limite de las dimensiones del campo de batalla
-            function graficadora(x1, y1, x2, y2, limite){//Matriz de las balas
-                var BalasDeLaMatriz = [new Array(50), new Array(50)];//sera el array de las balas la longitud que 
-                //recorreran sera la longitud del array
-                var m = (y2-y1)/(x2-x1);
-                for (var i=x1; i<(50+x1); i++){
-                ///el limite es de la matriz para que no haga un gasto innecesario de memoria
-                    if(i != limite){
-                        BalasDeLaMatriz[0][(i-x1)] = i;
-                        BalasDeLaMatriz[1][(i-x1)] = Math.round((m*(i))+y1);
-                    }else{
-                        i= 50+x1
-                    }
-           
-                }
-                return BalasDeLaMatriz;
+
+            function DistFromPlayer(matrizDondeSeTrabaja,y,x,rango){ //Nos da la distancia al jugador
+              var dist = Math.pow((matrizDondeSeTrabaja[0][1]-x),2) + Math.pow((matrizDondeSeTrabaja[0][2]-y),2);
+              dist = Math.sqrt(dist);
+              return dist;
             }
-        this.MatBalas = graficadora(this.WherX, this.WherY, this.ObjX, this.ObjY, this.limCampJue)
-        console.log(this.MatBalas);
-     }
-    MidnightBlame(){
-        var Coun = 0;
-        //the gods loop
-        //Posicion en donde debe comparar la matriz || la matriz del juego || 
-        function GodsLoop(MatBalas, MatOfGame, WhatIs, Coun){
-            setTimeout(function(){ 
-                if(Coun != 50){//Para que el bucle no se haga infinito
-                    if(WhatIs == 2){//para chcar si es nave enemiga o amiga
-                        if(MatBalas[0][Coun] == MatOfGame[0][1] && MatBalas[1][Coun] == MatOfGame[0][2]){
-                            console.log('impacto')
-                        }else{
-                        MatOfGame = MatrizThatMakeMeCry // las matriz del juego necesito hacer que sea la mas actualizada
-                            GodsLoop(MatBalas, MatOfGame, WhatIs, Coun);
-                        }                                              
-                    }else{
-                         for(i=1; i<MatOfGame.length; i++)//checar la matriz del juego para poder comparar si hay un impacto
-                            if(MatBalas[0][Coun]==MatOfGame[i][1] && MatBalas[1][Coun]==MatOfGame[i][2]){//saber si impactaste
-                                 console.log('impacto a una nave enemiga')///codigo del impacto
-                            }//no se cuento se alargue este, creo que hare otro objeto para la destruccion
-                            else{
-                            MatOfGame = MatrizThatMakeMeCry; //las matriz del juego necesito hacer que sea la mas actualizada
-                                GodsLoop(MatBalas, MatOfGame, WhatIs, Coun);
-                            }
-                              
-                    }
-                }     
-            },100);//velocidad de las balas se puede cambiar
-        }//llave del fin de la funcion
-//PARAMETROS || MatRIZbALAS Balas, Matratriz del juego, WhatIs, Coun
-/*
-  this.Matriz = PrincipalMat;//matris donde estan declaradas todas las naves
-      
-*/
-        GodsLoop(this.MatBalas, this.Matriz,  this.Killed, Coun);
-    }//llave del fin del metodo  
-}
+            function ChasePlayer(matrizDondeSeTrabaja,x,y){ //Nos dice como perseguir al jugador
+              var dy = matrizDondeSeTrabaja[0][2]-y;
+              var dx = matrizDondeSeTrabaja[0][1]-x;
+              var retur = 0;
+              if((Math.abs(dx) <= Math.abs(dy)) || dy ==0){
+                if(dx < 0)
+                  retur=3;
+                if(dx > 0)
+                  retur=4;
+              }
+              if((Math.abs(dy) < Math.abs(dx)) || dx == 0){
+                if(dy < 0)
+                  retur=1;
+                if(dy > 0)
+                  retur=2;
+              }
+              return retur;
+            }
+        //console.log(this.number)
+        MainBucle(1000, this.workingMat, this.workingMat[this.number][2], this.workingMat[this.number][1], this.numero, 1, 2, 3, 4, this.number,this.current_pattern, this.pattern_move,patterns);
+        }
+
+    }
 ////-------------------------------------------------------------------------------------------------------
 ////-------------------------------------------------------------------------------------------------------
 ////---------------------------------------------------------------------------------------------------
 //  value valie sirve para saber el indice donde se guardara el incie de la matriz que me hace llorar
-function ChooseWhereToMove(color,matriz,y, x, event, argu1, argu2, argu3, argu4, argu5, value){//switch para elegir 
+function ChooseWhereToMove(color,matriz,y, x, event, argu1, argu2, argu3, argu4, value){//switch para elegir
     //donde se va a mover cada nave, servira para la principal y para las naves enemigas
     LimpiarLaMatriz(y, x, "white");
-    //y que la nave pueda moverse   ----  inicioX     inicioY  
+    //y que la nave pueda moverse   ----  inicioX     inicioY
     switch(event){
         case argu1:if(y==0)
                         y=limit-1;
@@ -134,21 +139,14 @@ function ChooseWhereToMove(color,matriz,y, x, event, argu1, argu2, argu3, argu4,
         case argu3: if (x==0)
                         x = limit-1;
                     else
-                        x--;   
+                        x--;
             break;
         case argu4: if(x==limit-1)
                         x=0;
                     else
-                        x++;                 
+                        x++;
             break;
-        case argu5: if(color == "red"){//aqui ira el evento de disparo para las naves enemigas
-                        var ejecucion = new balas(MatrizThatMakeMeCry, x, y, MatrizThatMakeMeCry[0][1], MatrizThatMakeMeCry[0][1], 2, 50);
-                        ejecucion.MidnightBlame();
-                    }else{ //aqui el evento de disparo de nuestra nave, son diferentes por su algoritmo de disparo
-                        console.log("la nave disparo pero no puedo"); //se lo pondre con una parte de la interfaz grafica
-                    }             
-            break;
-    } 
+    }
     //ponerLasNavesEnLaMatriz(matriz);/////////////////////////////////Arreglar
     LimpiarLaMatriz(y, x, color);
     var regreso = new Array(2);
@@ -157,7 +155,7 @@ function ChooseWhereToMove(color,matriz,y, x, event, argu1, argu2, argu3, argu4,
    MatrizThatMakeMeCry[value][2] = y;//principl
    //console.log(MatrizThatMakeMeCry)
    return regreso
-  
+
 }
 function dibujarReticula(){//funcion que se puede quitar cuando se pase el juego
     ///Solo sirve aqui en canvas esta funcion se ira muy lejos jajajaja
@@ -206,7 +204,7 @@ function NumerosAleatorios(tope){
 function ArrayBaseDeLaNaves(numero){//declarando el array de las naves y sus posiciones
     var MatrizPrincipal = new Array(numero);
     for (let i = 0; i < numero; i++)
-        MatrizPrincipal[i]=new Array(3);//array que dira si es una nave enemiga y sus posiciones en X,Y 
+        MatrizPrincipal[i]=new Array(3);//array que dira si es una nave enemiga y sus posiciones en X,Y
     for (i=0; i<numero; i++){
         MatrizPrincipal[i][0] = 1;
         for (e=1; e<3; e++){
@@ -220,7 +218,7 @@ return MatrizPrincipal;
 function colocarPosicionesAleatorias(numNaves){//saber donde estaran las naves al inicio
     //tambien es medio la base de todo el juego espero que esto cambie
    // var Matriz = ArrayBaseDeLaNaves(numNaves);//tipo instanciando la matriz principal
-    ponerLasNavesEnLaMatriz(MatrizThatMakeMeCry)//ibujar la matriz de nuemeros en esta de canvas        
+    ponerLasNavesEnLaMatriz(MatrizThatMakeMeCry)//ibujar la matriz de nuemeros en esta de canvas
     //instanciando el objeto principal ----------------------
     const nave = new PersonajePrincipal(MatrizThatMakeMeCry);
     nave.mover()//haciendo que el objeto funcione  las teclas
@@ -229,18 +227,18 @@ function colocarPosicionesAleatorias(numNaves){//saber donde estaran las naves a
     for(let i = 0; i< ArrayObjetos.length; i++){//funcion para que los objetos se instancien con sus propiedades
         ArrayObjetos[i] = new NavesEnemigas(MatrizThatMakeMeCry, i+1);
         ArrayObjetos[i].JustTheCreator();//Js es una mamada jajaja
-    } 
+    }
 }
 ///-----------------------------------------------------------------------------------------------
 ///--------------------Ejecuciones----------------------------------------------------------------
 ///-----------------------------------------------------------------------------------------------
 //               Funcion principal que corre todo el juego
-colocarPosicionesAleatorias(numnaves)//esta ganando mucha importancia esta funcion 
+colocarPosicionesAleatorias(numnaves)//esta ganando mucha importancia esta funcion
 
 
 //------------------------------------------------------------------------------------------------
 //-------------PURO CAMVAS------- (matriz de los monitos)--------------------------------
 lienzoBase.beginPath();//EMPEZAR EL DIBUJO
-//dibujar la reticula 
-dibujarReticula();//esta es la reticula del 
+//dibujar la reticula
+dibujarReticula();//esta es la reticula del
 lienzoBase.closePath();
