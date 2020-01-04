4fd69c5c1d80bcd83f3b48c1e6195a0c5b036985
fer <fernandoalvarado@comunidad.unam.mx>
2020-01-03 23:01:02 -0600

    im still working at bullets

--- Basura que i need/ImDoingMagic.js
+++ Basura que i need/ImDoingMagic.js
@@ -32,30 +32,30 @@ class balas{
         this.Killed = Killed;///a que va a matar, si es nave enemiga o no
         this.limCampJue = limiteCampoJuego;///limite de las dimensiones del campo de batalla
             function graficadora(x1, y1, x2, y2, limite){//Matriz de las balas
-                var BalasDeLaMatriz = [new Array(50), new Array(50)];//sera el array de las balas la longitud que 
-                //recorreran sera la longitud del array
                 var m = (y2-y1)/(x2-x1);
-                for (var i=x1; i<(50+x1); i++){
-                ///el limite es de la matriz para que no haga un gasto innecesario de memoria
-                    if(i != limite){
-                        BalasDeLaMatriz[0][(i-x1)] = i;
-                        BalasDeLaMatriz[1][(i-x1)] = Math.round((m*(i))+y1);
+                const BalasDeLaMatriz = [new Array(0), new Array(0)];//sera el array de las balas la longitud que 
+                var i = x1;
+               while(i != limite || BalasDeLaMatriz[1][BalasDeLaMatriz[1].length-1] != limite){
+                    if(m <= 2.5){
+                        BalasDeLaMatriz[0].push = i;
+                        BalasDeLaMatriz[1].push = Math.round((m*(i-x1))+y1);//la primera locacion siempre sera la nave donde
+                        //se disparo
                     }else{
-                        i= 50+x1
+                        BalasDeLaMatriz[0].push = i*.1;
+                        BalasDeLaMatriz[1].push = Math.round(((m*(i-x1))+y1)*.1);
                     }
-           
-                }
+                       
+                    i++;
+               }//aqui se acaba la llave del while
                 return BalasDeLaMatriz;
             }
         this.MatBalas = graficadora(this.WherX, this.WherY, this.ObjX, this.ObjY, this.limCampJue)
      }
     MidnightBlame(){
         var Coun = 0;
-        //the gods loop
-        //Posicion en donde debe comparar la matriz || la matriz del juego || 
         function GodsLoop(MatBalas, MatOfGame, WhatIs, Coun){
             setTimeout(function(){ 
-                if(Coun != 50){//Para que el bucle no se haga infinito
+                if(Coun != MatBalas.length){//Para que el bucle no se haga infinito
                     if(WhatIs == 2){//para chcar si es nave enemiga o amiga
                         if(MatBalas[0][Coun] == MatOfGame[0][1] && MatBalas[1][Coun] == MatOfGame[0][2]){
                             console.log('impacto')
@@ -76,49 +76,36 @@ class balas{
                     }
                 }     
             },100);//velocidad de las balas se puede cambiar
-        }//llave del fin de la funcion
-//PARAMETROS || MatRIZbALAS Balas, Matratriz del juego, WhatIs, Coun
-        GodsLoop();
+        }//Where func ends   PARAMETROS || MatRIZbALAS Balas, Matratriz del juego, WhatIs, Coun
+        console.log("The object start");///solo para ver si sirve
+        GodsLoop(this.MatBalas, this.Matriz, this.Killed, Coun);
     }//llave del fin del metodo  
  }
 ////---------------------------------------------------------------------------------------------------
 ///----------------------------------------------------------------------------------------------------
 var tontaVariable = ArrayBaseDeLaNaves(8);
-console.log(tontaVariable.length)
-console.log("///////////////////////////////");
+
 //PrincipalMat, whereX, whereY, ObjX, ObjY, Killed, limiteCampoJuego
-var fernando = new balas(tontaVariable, 1,1,8,4,2,51)
-fernando.MidnightBlame();
-
-
-
-//console.log()
-
-
-
-//var Runni = ArrayBaseDeLaNaves(4);
-
-
-
-/*for (i=x1; i>=10; i++){
-    console.log((m*i)+y1)
-  }*/
-
-/*
-Crear una cookie
-document.cookie = “name=valor”;
-document.cookie = “name=valor; expires=fecha en UTC”;
-Obtener cookies
-document.cookie; //obtiene una cadena con todas las cookies
-///////--------------------------------------------------------------------------------------------
-///////--------------------------------------------------------------------------------------------
-class PersonajePrincipal{
-       constructor (matrizDondeSeTrabaja) {           
-            this.matrizDondeSeTrabaja = matrizDondeSeTrabaja;         
+//var fernando = new balas(tontaVariable, 1,1,8,4,2,51)
+//fernando.MidnightBlame();
+function graficadora(x1, y1, x2, y2, limite){//Matriz de las balas
+    var m = (y2-y1)/(x2-x1);
+    const BalasDeLaMatriz = [new Array(0), new Array(0)];//sera el array de las balas la longitud que 
+    var i = x1;
+   while(i != limite && BalasDeLaMatriz[1][BalasDeLaMatriz[1].length-1] != limite){
+        if(m <= 2.5){
+            BalasDeLaMatriz[0].push = i;
+            BalasDeLaMatriz[1].push = Math.round((m*(i-x1))+y1);//la primera locacion siempre sera la nave donde
+            //se disparo
+        }else{
+            BalasDeLaMatriz[0].push = i*.1;
+            BalasDeLaMatriz[1].push = Math.round(((m*(i-x1))+y1)*.1);
         }
-        mover(){
            
-         
-        }
-    }
-*/
+        i++;
+   }//aqui se acaba la llave del while
+    return BalasDeLaMatriz;
+}
+
+me = graficadora(1,1,2,3,50);
+console.log(me)
