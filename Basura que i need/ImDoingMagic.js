e19259b98973a4b84cfbde65a85af37d433ed3b7
fer <fernandoalvarado@comunidad.unam.mx>
2020-01-18 23:46:04 -0600

    Subiendo el evento que hara que las neves se muevan pa arriba y abajo con el mouse xd

--- Basura que i need/ImDoingMagic.js
+++ Basura que i need/ImDoingMagic.js
@@ -13,16 +13,25 @@ ctx.strokeRect(0,0,200,200);
 var coordenadasPantalla = document.getElementById('coordenadas');//variable para poner las posiciones que saco
 var AlturaPantalla = document.getElementById('Altura');
 var AnchoPantalla =  document.getElementById('Ancho');
+var subeOBaja = document.getElementById('subeOBaja');
 //---Otras variables que necesito
-var altura = screen.height;
-var ancho = screen.width;
+var altura = 200; //es la altura del canvas
+var ancho = 200; // es el ancho del canvas jajaja
 //--------------Here it ends
 AlturaPantalla.innerText=altura///Solo para representar la altura en pantalla
 AnchoPantalla.innerText=ancho//solo queiro saber que pedo con el nacho de la pantalla
 
 //Funciones de enventos, creo que solo necesito 2 y luego hare una funcion para que la nave suba o baje
-document.addEventListener('mousemove', function(event){
-    var WhereIsTheMouse = [event.pageX,event.pageY]
+canvas.addEventListener('mousemove', function(event){
+    var WhereIsTheMouse = [event.pageX - 10,event.pageY -8]//Lo que
+   if(WhereIsTheMouse[1] < (altura)/3)
+        var parametro = 0;
+    else if(WhereIsTheMouse[1] > ((altura)/3)*2)
+        var parametro = 1;
+    else
+        console.log('no debe pasar nada xd')
+   
+    subeOBaja.innerText= parametro;
 
   
     coordenadasPantalla.innerText= 'x= '+WhereIsTheMouse[0]+' Y= '+WhereIsTheMouse[1]
