2511dd9864e7ec7e7e1c82c32b54fac18d82a489
Diegolibot <diegolibot@comunidad.unam.mx>
2020-01-20 14:32:03 -0600

    Subiendo cosas de fer

--- Basura que i need/ImDoingMagic.js
+++ Basura que i need/ImDoingMagic.js
@@ -14,9 +14,12 @@ var coordenadasPantalla = document.getElementById('coordenadas');//variable para
 var AlturaPantalla = document.getElementById('Altura');
 var AnchoPantalla =  document.getElementById('Ancho');
 var subeOBaja = document.getElementById('subeOBaja');
+var rotcionCamara = document.getElementById('rotacionCamara')
+var vistasDefinidas = [0,90,180,270];//todos los valores que puede tomar la camara
 //---Otras variables que necesito
 var altura = 200; //es la altura del canvas
-var ancho = 200; // es el ancho del canvas jajaja
+var ancho = 200 
+var gradosPantalla = 0; //Estos son los grdos donde inicialmnte empieza a ver e tipo
 //--------------Here it ends
 AlturaPantalla.innerText=altura///Solo para representar la altura en pantalla
 AnchoPantalla.innerText=ancho//solo queiro saber que pedo con el nacho de la pantalla
@@ -27,12 +30,13 @@ canvas.addEventListener('mousemove', function(event){
    if(WhereIsTheMouse[1] < (altura)/3)
         var parametro = 0;
     else if(WhereIsTheMouse[1] > ((altura)/3)*2)
-        var parametro = 1;
-    else
-        console.log('no debe pasar nada xd')
-   
+        var parametro = 1;   
     subeOBaja.innerText= parametro;
-
+    if(WhereIsTheMouse[0]< (altura)/4)//girara a la derecha
+    setTimeout(function(){ 
+        gradosPantalla -= 90
+        rotcionCamara.innerText= gradosPantalla;
+    }, 1000);
   
     coordenadasPantalla.innerText= 'x= '+WhereIsTheMouse[0]+' Y= '+WhereIsTheMouse[1]
     return WhereIsTheMouse;
