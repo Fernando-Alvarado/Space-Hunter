9347d85c4436d88b824a8ca911408ffaf2696c44
caninazo2827 <alberto130205@gmail.com>
2020-02-27 04:31:16 -0600

    soy un pendejo

--- SpaceHunter/Dynamics/php/buscar_usuario.php
+++ SpaceHunter/Dynamics/php/buscar_usuario.php
@@ -7,7 +7,7 @@
     $array_JSON=getArrayFromJSON();
 
     $index=searchNickname($array_JSON,$datos_formu['nick']);
-    if($index=!-1)
+    if($index!=-1)
     {
         if($array_JSON[$index]['password']==$datos_formu['pass'])
             echo true;
